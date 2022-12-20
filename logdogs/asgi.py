7ab665ed51f01e37ca4f96316980a9202e2fe5f7
Uzair Ali <uzaxirr@gmail.com>
2022-12-21 00:29:43 +0530

    black format

--- logdogs/asgi.py
+++ logdogs/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'logdogs.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "logdogs.settings")
 
 application = get_asgi_application()
